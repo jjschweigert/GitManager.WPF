9c666ff3ed0486349dff93683da1e7b729318bd9
jjschweigert <jjschweigert.personal@gmail.com>
2019-06-16 21:36:30 -0400

    Added example project for drawer navigation in WPF. Added grid splitter to example.

---
+++ GitManager.WPF.Playground/GitManager.WPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitManager.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+        }
+
+        private async void CloseGridMenu_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(500);
+            GridMenu.Visibility = Visibility.Collapsed;
+            ViewSplitter.Visibility = Visibility.Visible;
+        }
+
+        private void OpenGridMenu_Click(object sender, RoutedEventArgs e)
+        {
+            GridMenu.Visibility = Visibility.Visible;
+            ViewSplitter.Visibility = Visibility.Collapsed;
+        }
+    }
+}
