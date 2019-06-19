f8b103bf0c2c17d24c67592b177b7ff7e732317a
= <=>
2019-06-19 00:32:40 -0400

    UI is in working state, need to create main window view model, need to decide on overlay (sidebar currently).

--- GitManager.WPF/MainWindow.xaml.cs
+++ GitManager.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,83 @@ using System.Windows.Shapes;
 
 namespace GitManager.WPF
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ViewModel TestViewModel { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            TestViewModel = new ViewModel();
+            DataContext = TestViewModel;
+        }
+
+        private async void Close_Sidebar_Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(500);
+            TestViewModel.Content_GridSplitter_Visibility = Visibility.Visible;
+            TestViewModel.Sidebar_Grid_Visibility = Visibility.Collapsed;
+        }
+
+        private void Open_Sidebar_Button_Click(object sender, RoutedEventArgs e)
+        {
+            TestViewModel.Content_GridSplitter_Visibility = Visibility.Collapsed;
+            TestViewModel.Sidebar_Grid_Visibility = Visibility.Visible;
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Visibility _Content_GridSplitter_Visibility { get; set; }
+        public Visibility Content_GridSplitter_Visibility
+        {
+            get
+            {
+                return _Content_GridSplitter_Visibility;
+            }
+            set
+            {
+                _Content_GridSplitter_Visibility = value;
+                NotifyPropertyChanged("Content_GridSplitter_Visibility");
+            }
+        }
+
+        private Visibility _Sidebar_Grid_Visibility { get; set; }
+        public Visibility Sidebar_Grid_Visibility
+        {
+            get
+            {
+                return _Sidebar_Grid_Visibility;
+            }
+            set
+            {
+                _Sidebar_Grid_Visibility = value;
+                NotifyPropertyChanged("Sidebar_Grid_Visibility");
+            }
+        }
+
+        public string WindowTitle
+        {
+            get
+            {
+                return "Local Git Manager";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+            _Sidebar_Grid_Visibility = Visibility.Collapsed;
+            _Content_GridSplitter_Visibility = Visibility.Visible;
+        }
+
+        public void NotifyPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
     }
 }
