5c43d97ae81f98aae32896c811e275e122bf6204
jjschweigert <jjschweigert.developer@gmail.com>
2019-10-02 22:17:55 -0400

    Added example UI to modify

--- GitManager.WPF/App.xaml.cs
+++ GitManager.WPF/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Views;
 
 namespace GitManager.WPF
 {
@@ -15,7 +16,21 @@ namespace GitManager.WPF
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            
+            Window MainWindow = new Window
+            {
+                Height = 600,
+                MinHeight = 600,
+                MaxHeight = 600,
+                Width = 1024,
+                MinWidth = 1024,
+                MaxWidth = 1024,
+                WindowStyle = WindowStyle.None,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+
+            MainWindow.Content = new MainView();
+            MainWindow.Show();
+
         }
     }
 }
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Page
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Shutdown();
+        }
+    }
+}
