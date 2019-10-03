66e94275ca55036b287a80268d994eb549a5c14f
jjschweigert <jjschweigert.developer@gmail.com>
2019-10-03 00:28:04 -0400

    Modified example UI to include an object animation with a discrete keyframe to change a setter on mouse over.

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -39,7 +40,48 @@ namespace Views
 
         private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
-            //Application.Current.Shutdown();
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonCloseMenu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //if (ButtonCloseMenu.Visibility == Visibility.Visible)
+            //{
+            //    ButtonCloseMenu.Effect = new DropShadowEffect
+            //    {
+            //        ShadowDepth = 5,
+            //        BlurRadius = 5,
+            //        Direction = 5,
+            //        Opacity = 100,
+            //        Color = Colors.Black
+            //    };
+            //}
+        }
+
+        private void ButtonCloseMenu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //if(ButtonCloseMenu.Visibility == Visibility.Visible)
+            //    ButtonCloseMenu.Effect = null;
+        }
+
+        private void ButtonOpenMenu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //if (ButtonOpenMenu.Visibility == Visibility.Visible)
+            //{
+            //    ButtonOpenMenu.Effect = new DropShadowEffect
+            //    {
+            //        ShadowDepth = 0,
+            //        BlurRadius = 20,
+            //        Opacity = 100,
+            //        Color = Colors.Black
+            //    };
+            //}
+        }
+
+        private void ButtonOpenMenu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //if (ButtonOpenMenu.Visibility == Visibility.Visible)
+            //    ButtonOpenMenu.Effect = null;
         }
     }
 }
